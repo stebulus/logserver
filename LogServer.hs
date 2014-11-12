08f043817b8d8fe39a33ddabf97ade7c3beb6d86
Steven Taschuk <steven@amotlpaa.org>
2014-11-12 10:36:50 -0500

    Add logging to a file specified on the command line.

--- LogServer.hs
+++ LogServer.hs
@@ -1,17 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Concurrent.MVar (newMVar, takeMVar, putMVar, MVar)
+import Control.Exception (bracket)
+import Data.ByteString (hPut)
 import Data.Maybe (listToMaybe)
-import Network.Wai (Application, responseLBS)
-import Network.HTTP.Types (status200)
+import Network.Wai
+    (Application, responseLBS, requestMethod, requestBody, Request)
+import Network.HTTP.Types (status200, status405, methodPost)
 import Network.Wai.Handler.Warp (run)
 import System.Environment (getArgs, getProgName)
 import System.Exit (exitWith, ExitCode(..))
-import System.IO (stderr, hPutStrLn)
+import System.IO (stderr, hPutStrLn, withFile, IOMode(AppendMode), Handle)
 
-app :: Application
-app _ respond = respond $ responseLBS
-    status200
-    [("Content-Type", "text/plain")]
-    "Hello, World!"
+copyChunk :: Request -> Handle -> IO Bool
+copyChunk req h = do
+    chunk <- requestBody req
+    if chunk == ""
+      then return False
+      else do
+        hPut h chunk
+        return True
+
+copyAll :: Request -> Handle -> IO ()
+copyAll req h = do
+    result <- copyChunk req h
+    if result
+      then copyAll req h
+      else return ()
+
+app :: MVar Handle -> Application
+app log req respond =
+    if requestMethod req == methodPost
+      then
+        bracket (takeMVar log)
+                (\h -> putMVar log h)
+                (\h -> do copyAll req h
+                          respond $ responseLBS
+                            status200
+                            [("Content-Type", "text/plain")]
+                            "Logged.")
+      else
+        respond $ responseLBS
+            status405
+            [("Allow", "POST"), ("Content-Type", "text/plain")]
+            "Only POST to this server."
 
 maybeRead :: (Read a) => String -> Maybe a
 maybeRead s = do
@@ -22,10 +53,10 @@ maybeRead s = do
 
 main = do
     args <- getArgs
-    if length args /= 1
+    if length args /= 2
       then do
         name <- getProgName
-        hPutStrLn stderr $ "usage: " ++ name ++ " port"
+        hPutStrLn stderr $ "usage: " ++ name ++ " port filename"
         exitWith $ ExitFailure 2
       else do
         case maybeRead (args!!0) of
@@ -35,4 +66,7 @@ main = do
             exitWith $ ExitFailure 2
           Just port -> do
             putStrLn $ "binding to port " ++ show port
-            run port app
+            withFile (args!!1) AppendMode $ \h -> do
+                putStrLn $ "logging to file " ++ (args!!1)
+                mh <- newMVar h
+                run port (app mh)
