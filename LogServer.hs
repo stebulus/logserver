49e7dcf367dea53775a9a119ccc187f7ae0c1ce0
Steven Taschuk <steven@amotlpaa.org>
2014-11-12 09:48:39 -0500

    Put port number on command line.

--- LogServer.hs
+++ LogServer.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Data.Maybe (listToMaybe)
 import Network.Wai (Application, responseLBS)
 import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitWith, ExitCode(..))
+import System.IO (stderr, hPutStrLn)
 
 app :: Application
 app _ respond = respond $ responseLBS
@@ -9,4 +13,26 @@ app _ respond = respond $ responseLBS
     [("Content-Type", "text/plain")]
     "Hello, World!"
 
-main = run 3000 app
+maybeRead :: (Read a) => String -> Maybe a
+maybeRead s = do
+    (a,xs) <- listToMaybe $ reads s
+    case xs of
+        "" -> Just a
+        otherwise -> Nothing
+
+main = do
+    args <- getArgs
+    if length args /= 1
+      then do
+        name <- getProgName
+        hPutStrLn stderr $ "usage: " ++ name ++ " port"
+        exitWith $ ExitFailure 2
+      else do
+        case maybeRead (args!!0) of
+          Nothing -> do
+            hPutStrLn stderr $
+                "error: cannot interpret " ++ args!!0 ++ " as an integer"
+            exitWith $ ExitFailure 2
+          Just port -> do
+            putStrLn $ "binding to port " ++ show port
+            run port app
