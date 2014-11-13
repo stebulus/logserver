93dfe30153127220463646c3e2ca941769db93cc
Steven Taschuk <steven@amotlpaa.org>
2014-11-13 12:25:03 -0500

    Require submissions to have Content-Type: text/*

--- LogServer.hs
+++ LogServer.hs
@@ -1,13 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Codec.MIME.Type (Type(..), MIMEType(..))
+import Codec.MIME.Parse (parseContentType)
 import Control.Concurrent.MVar (newMVar, takeMVar, putMVar, MVar)
 import Control.Exception (bracket)
 import Control.Monad (when)
 import Data.ByteString (empty, hPut)
-import Data.Maybe (listToMaybe)
+import Data.ByteString.Lazy (fromStrict)
+import qualified Data.CaseInsensitive as CI
+import Data.Maybe (listToMaybe, fromMaybe)
+import Data.Monoid (mconcat)
+import Data.Text.Encoding (decodeUtf8')
 import Data.Version (showVersion)
-import Network.Wai
-    (Application, responseLBS, requestMethod, requestBody, Request)
-import Network.HTTP.Types (status200, status405, methodPost)
+import Network.Wai (Application, responseLBS, requestMethod, requestBody,
+    requestHeaders, Request)
+import Network.HTTP.Types (status200, status400, status415, status405,
+    methodPost)
 import Network.Wai.Handler.Warp
     (runSettings, setHost, setPort, defaultSettings, Port)
 import System.Environment (getArgs, getProgName)
@@ -39,6 +46,18 @@ app log req respond =
              status405  -- Method Not Allowed
              [("Allow", "POST"), ("Content-Type", "text/plain")]
              "Only POST to this server.\r\n"
+        case fmap mimeType contentType of
+            Nothing -> bad status400  -- Bad Request
+                           [("Content-Type", "text/plain")]
+                           $ mconcat [ "Incomprehensible Content-Type: "
+                                     , contentTypeBS
+                                     , "\r\n" ]
+            Just (Text _) -> continue
+            _ -> bad status415  -- Unsupported Media Type
+                     [("Content-Type", "text/plain")]
+                     $ mconcat [ "Submit text/* to this server, not "
+                               , contentTypeBS
+                               , "\r\n" ]
         return $
             bracket (takeMVar log)
                     (putMVar log)
@@ -53,6 +72,12 @@ app log req respond =
     where bad status headers body =
             Left $ respond $ responseLBS status headers body
           continue = Right (return () :: IO ())
+          contentTypeBSS = fromMaybe "application/octet-stream"
+                           $ lookup (CI.mk "Content-Type") (requestHeaders req)
+                           -- default content-type is application/octet-stream
+                           -- per RFC 2616 section 7.2.1
+          contentTypeBS = fromStrict contentTypeBSS
+          contentType = (right $ decodeUtf8' contentTypeBSS) >>= parseContentType
 
 while :: (a->Bool) -> IO a -> (a->IO ()) -> IO ()
 while pred io act = loop
@@ -67,6 +92,10 @@ maybeRead :: (Read a) => String -> Maybe a
 maybeRead s = do
     (a,unparsed) <- listToMaybe $ reads s
     if unparsed == "" then Just a else Nothing
+
+right :: Either a b -> Maybe b
+right (Left _) = Nothing
+right (Right b) = Just b
 
 usage :: String -> String
 usage progname = "usage: " ++ progname ++ " port filename\n\
