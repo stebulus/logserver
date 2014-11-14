4510b40f95b5e2e8a56b26f275ba02120ce046c1
Steven Taschuk <steven@amotlpaa.org>
2014-11-14 08:47:44 -0500

    Report errors in greaselog.js

--- greaselog.js
+++ greaselog.js
@@ -7,13 +7,22 @@
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
 
+function alertFail(resp) {
+    alert("Error!  " + resp.status + " " + resp.statusText
+                     + "\n" + resp.responseText);
+}
+
 GM_registerMenuCommand(
     "Log this page",
     function () {
         GM_xmlhttpRequest({
             method: "POST",
             url: "http://localhost:3042/",
-            data: "* [" + content.document.title + "](" + content.document.location + ")\r\n"
+            data: "* [" + content.document.title + "](" + content.document.location + ")\r\n",
+            onerror: alertFail,
+            onload: function (resp) {
+                if (resp.status!="200") alertFail(resp);
+            }
         })
     },
     "l")
